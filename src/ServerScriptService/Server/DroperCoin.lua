fb1d63bc7ac51e4b86966fdfb7f7c3fc1f0f021f
vladislovv <vlad.yelcheninov@gmail.com>
2024-01-31 11:27:03 +0300

    asdfasdf

--- src/ServerScriptService/Server/DroperCoin.lua
+++ src/ServerScriptService/Server/DroperCoin.lua
@@ -3,16 +3,46 @@ local DroperCoin = {}
 local RS = game:GetService('ReplicatedStorage')
 local Remote = RS:WaitForChild('Remote')
 local TWS = RS:WaitForChild('Libary')
-local CoinFolder = game.Workspace.RandomToken
+local CoinFolder = game.Workspace.RandomToken:GetChildren()
+local Couldown = 5 -- 450
+local maxCoins = 5
+local CoinSpawn = 0
 
 local CollectCoin = 0
-for _, index in next, CoinFolder:GetChildren() do
-	--как сделать подсчет сколько в цифрах обьктов в папке через скрипт   
-    if index:IsA("MeshPart") then
-        CollectCoin = CollectCoin + 1
+
+function CountCoins()
+    CoinSpawn = 0
+    for _, Coin in next, CoinFolder do
+        if CoinSpawn == maxCoins then
+            if Coin:FindFirstChild("Coin") then
+                CoinSpawn += 1
+            end 
+        end
     end
-	print(CollectCoin)
-    --// дописать до конца
+    return CoinSpawn
 end
+
+function CoinSpawner()
+    while true do
+        local amount = CountCoins()
+
+        if amount <= maxCoins then
+            local CoinClone = RS.CoinClone:Clone()
+            local RandomSpawn
+
+            repeat
+                RandomSpawn = CoinFolder[math.random(1, #CoinFolder)]
+            until not RandomSpawn:FindFirstChild('Coin')
+            
+            CoinClone.Transparency = 0
+            CoinClone.Parent = RandomSpawn
+            CoinClone.CFrame = RandomSpawn.CFrame + Vector3.new(0,3,0)
+            print("Spawn")
+        end
+        task.wait(Couldown)
+    end
+end
+
+task.spawn(CoinSpawner)
 
 return DroperCoin 
